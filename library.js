60e9f14fb6cb62f2325506896a4032e1c71781f6
KushPatel86 <kpatel86@villanova.edu>
2024-04-23 11:45:01 -0400

    just standardizing method parameters
    This is also just a simple first push by me to see

--- library.js
+++ library.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { FlatList, StyleSheet, Text, TouchableOpacity, View, Button } from 'react-native';
 
-export default function Library( navigation, route ) {
-
+export default function Library( route, navigation ) {
+    
 }
