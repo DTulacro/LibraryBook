1aac7b513872049c1af15ced9c1d9e9937cc2856
DTulacro <d.tulacro@icloud.com>
2024-04-23 12:30:38 -0400

    new library

--- library.js
+++ library.js
@@ -1,3 +1,53 @@
-export default function Library( navigation, route ) {
+import React, { useState, useEffect } from 'react';
+import { TouchableOpacity, Button, View, FlatList, Text } from 'react-native';
+
+export default function Library( navigation ) {
+
+    const startingData = [];
+
+    const [books, setBooks] = useState(startingData);
+
+    function getBooks() {
+        let tempList = [];
+        fetch('https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=F93caSWCo07hIFLfgYl0BTojagbBVQRR')
+        .then((response) => response.json())
+        .then((json) => {  
+          /* view the JSON that's returned in the server log */ 
+            console.log(json);
+            for(i = 0; i < 15; i++) {
+                let temp = {key: i, 
+                            title: json.results.books[i].title,
+                            author: json.results.books[i].author, 
+                            description: json.results.books[i].description, 
+                            isbn: json.results.books[i].primary_isbn13};
+                tempList.push(temp);
+            }
+
+            setBooks(books.concat(tempList));
+        })
+        .catch((error) => {
+        console.error(error);
+        });
+        
+    }
+    
+    function addBooks () {
+
+    }
+
+    return (
+        <View>
+        <FlatList data={books} extraData={books}
+        renderItem={({item}) => 
+          <View>
+            <TouchableOpacity onPress={ () => navigation.navigate('Information', {title: item.title}) }>
+              <Text>{item.title}</Text>
+            </TouchableOpacity>
+          </View>
+        } 
+        />
+        <Button title="Add Books" onPress={ getBooks }/>
+        </View>
+    );
 
 }
