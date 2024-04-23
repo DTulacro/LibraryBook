e6ead5c95232cdadde59fd608f9bcb747da8fd44
Jacob El-Zahr <jelzahr@villanova.edu>
2024-04-23 12:00:21 -0400

    skeleton of details
    preliminary outline for details.js

--- details.js
+++ details.js
@@ -1,3 +1,28 @@
-export default function Details( navigation, route ) {
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
+export default function Details( navigation, route ) {
+    const {title, releaseYear} = route.params;
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.item}>{title}</Text>    
+            <Text style={styles.item}>{releaseYear}</Text>
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+     paddingTop: 50
+    },
+    item: {
+      padding: 10,
+      fontSize: 18,
+      height: 40,
+    },
+    border: {
+      borderWidth: 1,
+      borderColor: "gray",
+    }
+  });  
