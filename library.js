2014a37e4fa8612b6f836c3c4db579e7bcfc354d
KushPatel86 <kpatel86@villanova.edu>
2024-04-23 19:55:44 -0400

    Added a button to library that redirects page to gemini upon click

--- library.js
+++ library.js
@@ -34,6 +34,7 @@ export default function Library( navigation ) {
 
     return (
         <View>
+          <Button title="Ask Gemini" onPress={()=>navigation.navigate('Gemini')}/>
         <FlatList data={books} extraData={books}
         renderItem={({item}) => 
           <View>
